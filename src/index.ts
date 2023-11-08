f721e9eb7aba2686cf5aa092bc7f432714fdcad0
Rizki Ramadhan <rizkijitu77@gmail.com>
2023-11-08 11:04:46 +0700

    fix : fix deployment

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response, json } from "express";
+import { ENV } from "./config";
+import cors from "cors";
+
+const PORT = ENV.PORT;
+
+const app: Express = express();
+
+import allRoutes from "./routes";
+
+// Route
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world 🌏");
+});
+
+// connect to DB
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+
+app.use(json());
+app.use(allRoutes);
+
+app.listen(PORT, () => {
+  console.log("listening on PORT on " + PORT);
+});
